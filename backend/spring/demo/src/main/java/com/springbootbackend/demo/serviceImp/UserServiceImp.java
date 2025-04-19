fea1b86a0df8fbc71b500ea30d2ce0f65f732ce9
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-04-19 14:01:42 +0530

    implementation create and read methods

--- backend/spring/demo/src/main/java/com/springbootbackend/demo/serviceImp/UserServiceImp.java
+++ backend/spring/demo/src/main/java/com/springbootbackend/demo/serviceImp/UserServiceImp.java
@@ -1,6 +1,9 @@
 package com.springbootbackend.demo.serviceImp;
 
+import com.springbootbackend.demo.dto.UserDto;
+import com.springbootbackend.demo.model.Contact;
 import com.springbootbackend.demo.model.UserModel;
+import com.springbootbackend.demo.repository.ContactRepo;
 import com.springbootbackend.demo.repository.UserRepo;
 import com.springbootbackend.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +13,42 @@ import org.springframework.stereotype.Service;
 public class UserServiceImp implements UserService {
     @Autowired
     UserRepo userRepo;
-    @Override
-    public String createUser(String name, int age, String email, String phoneNo, String gender) {
-        UserModel userEntity = new UserModel();
-        userEntity.setName(name);
-        userEntity.setAge(age);
-        userEntity.setEmail(email);
-        userEntity.setGender(gender);
-        userEntity.setPhoneNo(phoneNo);
-        userRepo.save(userEntity);
+    @Autowired
+    ContactRepo contactRepo;
 
-        return userEntity.getId();
+    @Override
+    public String createUser(UserDto userDto) {
+        UserModel user = new UserModel();
+
+        user.setName(userDto.getName());
+        user.setAge(userDto.getAge());
+        user.setEmail(userDto.getEmail());
+        user.setPhoneNo(userDto.getPhoneNo());
+        user.setGender(userDto.getGender());
+        user.setHeight(userDto.getHeight());
+        user.setWeight(userDto.getWeight());
+        user.setDateOfBirth(userDto.getDateOfBirth());
+        user.setMaritalStatus(userDto.getMaritalStatus());
+        user.setBloodGroup(userDto.getBloodGroup());
+        user.setAllergies(userDto.getAllergies());
+        user.setMedicalConditions(userDto.getMedicalConditions());
+        user.setMedications(userDto.getMedications());
+        user.setAddress(userDto.getAddress());
+        user.setCity(userDto.getCity());
+        user.setState(userDto.getState());
+        user.setCountry(userDto.getCountry());
+        user.setZipCode(userDto.getZipCode());
+
+        if (userDto.getEmergencyContactId() != null) {
+
+            Contact emergencyContact = contactRepo.findById(userDto.getEmergencyContactId()).orElse(null);
+            user.setEmergencyContact(emergencyContact);
+        }
+
+        UserModel savedUser = userRepo.save(user);
+        return "User created with ID: " + savedUser.getId();
     }
+
+
+
 }
