08c8760229228ba36fff5fe47952a0855a845fc4
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-04-19 14:02:17 +0530

    service class for user and contact

---
+++ backend/spring/demo/src/main/java/com/springbootbackend/demo/service/ContactService.java
@@ -0,0 +1,8 @@
+package com.springbootbackend.demo.service;
+
+import com.springbootbackend.demo.dto.ContactDto;
+
+public interface ContactService {
+    public String createContact(ContactDto contactDto);
+
+}
