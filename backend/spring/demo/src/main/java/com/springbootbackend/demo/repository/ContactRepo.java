15893d107eba1fa49539b5a0467e2d856433ef93
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-04-19 21:44:50 +0530

    repository

--- backend/spring/demo/src/main/java/com/springbootbackend/demo/repository/ContactRepo.java
+++ backend/spring/demo/src/main/java/com/springbootbackend/demo/repository/ContactRepo.java
@@ -3,6 +3,11 @@ package com.springbootbackend.demo.repository;
 import com.springbootbackend.demo.model.Contact;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
 @Repository
 public interface ContactRepo extends  MongoRepository<Contact , String> {
+    List<Contact> findByUserId_Id(String userId);
+
 }
--- backend/spring/demo/src/main/java/com/springbootbackend/demo/repository/MedicalRecordRepo.java
+++ backend/spring/demo/src/main/java/com/springbootbackend/demo/repository/MedicalRecordRepo.java
@@ -4,6 +4,9 @@ import com.springbootbackend.demo.model.MedicalRecord;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MedicalRecordRepo extends MongoRepository<MedicalRecord , String> {
+    List<MedicalRecord> findByUserId_Id(String userId);
 }
